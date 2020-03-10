ba8a1f7cb14d7d15e88b020eedc0f0891bccb9f3
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-11 00:19:24 +0900

    :sparkles: (default.vue) Import app bar to default layout

--- packages/client/src/layouts/default.vue
+++ packages/client/src/layouts/default.vue
@@ -1,11 +1,17 @@
 <template>
   <v-app>
+    <the-app-bar />
     <v-content>
       <nuxt />
     </v-content>
   </v-app>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+export default createComponent({
+  components: {
+    TheAppBar: () => import('~/components/organisms/TheAppBar.vue')
+  }
+})
 </script>
