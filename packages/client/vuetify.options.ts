df45a4427508e6c5917dd66bbc8236c132bc19f7
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-11 00:39:54 +0900

    :wrench: (vuetify.options.ts) Add vuetify options file

---
+++ packages/client/vuetify.options.ts
@@ -0,0 +1,29 @@
+// import colors from 'vuetify/lib/util/colors'
+export default {
+  icons: {
+    iconfont: 'mdiSvg' // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+  },
+
+  theme: {
+    themes: {
+      light: {
+        primary: '#EF5350',
+        secondary: '#66BB6A',
+        accent: '#84FFFF',
+        error: '#AB47BC',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      },
+      dark: {
+        primary: '#1976D2',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      }
+    }
+  }
+}
