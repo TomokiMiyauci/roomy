d52255e512c618eaf25d20839796848c1d69be2e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-16 00:26:45 +0900

    :sparkles: (firebase.ts) Add plugin of firebase

---
+++ packages/client/src/plugins/firebase.ts
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// import 'firebase/storage'
+import 'firebase/auth'
+// import 'firebase/messaging'
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export default firebase
+export const firestore = firebase.firestore()
+export const auth = firebase.auth()
+// export const storage = firebase.storage()
