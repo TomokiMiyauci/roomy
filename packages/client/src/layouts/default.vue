f4cd3cd6297ffc440e542757683ae65d77fd8d70
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-16 00:10:15 +0900

    :sparkles: (default.vue) Add page transition

--- packages/client/src/layouts/default.vue
+++ packages/client/src/layouts/default.vue
@@ -2,16 +2,30 @@
   <v-app>
     <the-app-bar />
     <v-content>
-      <nuxt />
+      <transition name="fade">
+        <nuxt />
+      </transition>
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import { createComponent } from '@vue/composition-api'
+import TheAppBar from '@/components/organisms/TheAppBar.vue'
+
 export default createComponent({
   components: {
-    TheAppBar: () => import('~/components/organisms/TheAppBar.vue')
+    TheAppBar
   }
 })
 </script>
+
+<style lang="scss">
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 2s;
+}
+</style>
