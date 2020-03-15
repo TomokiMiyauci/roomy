d364f436a685f661a4f2689535f09b504e86b48e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-16 00:29:42 +0900

    :art: Change alias to @

--- packages/client/src/utils/store-accessor.ts
+++ packages/client/src/utils/store-accessor.ts
@@ -1,6 +1,6 @@
 import { Store } from 'vuex'
 import { getModule } from 'vuex-module-decorators'
-import Firestore from '~/store/firestore'
+import Firestore from '@/store/firestore'
 
 // eslint-disable-next-line import/no-mutable-exports
 let firestore: Firestore
