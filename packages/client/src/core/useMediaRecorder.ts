18faf0ebdb80ab284418f79dfa7053545481c850
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-18 00:02:40 +0900

    :recycle: (useMediaRecorder.ts) Change interface, add media stream setter

--- packages/client/src/core/useMediaRecorder.ts
+++ packages/client/src/core/useMediaRecorder.ts
@@ -1,9 +1,8 @@
-import { reactive, toRefs, onBeforeMount, watch } from '@vue/composition-api'
+import { reactive, toRefs } from '@vue/composition-api'
 
 type State = {
   isRecording: boolean
   blob: Blob | undefined
-  mediaStream: MediaStream | undefined
   mediaRecorder: MediaRecorder | undefined
 }
 
@@ -11,37 +10,24 @@ const useMediaRecorder = () => {
   const state = reactive<State>({
     isRecording: false,
     blob: undefined,
-    mediaStream: undefined,
     mediaRecorder: undefined
   })
 
-  onBeforeMount(async () => {
-    console.log(1)
+  const setMediaStream = (mediaStream: MediaStream) => {
+    state.mediaRecorder = new MediaRecorder(mediaStream)
 
-    state.mediaStream = await navigator.mediaDevices.getUserMedia({
-      audio: true
-    })
-  })
-
-  watch(
-    () => state.mediaStream,
-    (now) => {
-      if (!now) return
-      state.mediaRecorder = new MediaRecorder(state.mediaStream!)
-
-      state.mediaRecorder!.onstart = () => {
-        state.isRecording = true
-      }
-
-      state.mediaRecorder!.onstop = () => {
-        state.isRecording = false
-      }
-
-      state.mediaRecorder!.ondataavailable = (blobEvent) => {
-        state.blob = blobEvent.data
-      }
+    state.mediaRecorder.onstart = () => {
+      state.isRecording = true
     }
-  )
+
+    state.mediaRecorder.onstop = () => {
+      state.isRecording = false
+    }
+
+    state.mediaRecorder.ondataavailable = (blobEvent) => {
+      state.blob = blobEvent.data
+    }
+  }
 
   const start = () => {
     state.mediaRecorder!.start()
@@ -51,7 +37,7 @@ const useMediaRecorder = () => {
     state.mediaRecorder!.stop()
   }
 
-  return { ...toRefs(state), start, stop }
+  return { ...toRefs(state), start, stop, setMediaStream }
 }
 
 export default useMediaRecorder
