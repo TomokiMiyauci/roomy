c8974d52ebe8e51c73b40b37e8897c941a5dfc23
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-22 14:30:40 +0900

    :sparkles: (ChipFileSize.vue) Add atoms of chip for display file size

---
+++ packages/client/src/components/atoms/ChipFileSize.vue
@@ -0,0 +1,29 @@
+<template>
+  <base-chip small label>
+    <v-icon small left>{{ mdiFileImage }}</v-icon
+    >{{ size | prettyBytes }}
+  </base-chip>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+import { mdiFileImage } from '@mdi/js'
+import BaseChip from '@/components/atoms/BaseChip.vue'
+
+export default createComponent({
+  props: {
+    size: {
+      type: Number,
+      required: true
+    }
+  },
+
+  components: {
+    BaseChip
+  },
+
+  setup() {
+    return { mdiFileImage }
+  }
+})
+</script>
