cc5d18ca177ca9556a2391b5ce425ccea75485ca
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-22 14:32:35 +0900

    :sparkles: (BaseChip.vue) Add base component of chip

---
+++ packages/client/src/components/atoms/BaseChip.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-chip v-bind="$attrs" v-on="$listeners">
+    <slot />
+  </v-chip>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+export default createComponent({})
+</script>
