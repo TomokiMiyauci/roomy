ae6e31b2fb5d9946a3e867a31e5f55b457a2d856
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-22 14:35:45 +0900

    :sparkles: (MessageSet.vue) Add audio and image display space

--- packages/client/src/components/molecules/MessageSet.vue
+++ packages/client/src/components/molecules/MessageSet.vue
@@ -1,35 +1,84 @@
 <template>
-  <div>
+  <v-row :justify="isOwn ? 'start' : 'end'">
     <template v-if="isOwn">
-      <v-avatar color="indigo" right size="36">
-        <span class="white--text headline">36</span>
-      </v-avatar>
-      <box-callout :left="true">{{ message.text }}</box-callout>
-      <span class="ml-2 caption">{{ message.createdAt | time }}</span>
+      <v-col cols="auto" align-self="end">
+        <v-avatar color="indigo" right size="36">
+          <span class="white--text headline">36</span>
+        </v-avatar>
+      </v-col>
+
+      <v-col cols="auto" align-self="end">
+        <template v-if="message.kind === 'TEXT'">
+          <box-callout :left="true">{{ message.text }}</box-callout>
+        </template>
+
+        <template v-else-if="message.kind === 'AUDIO'">
+          <box-callout :left="true">{{ message.audioURL }}</box-callout>
+        </template>
+
+        <span class="ml-2 caption">{{ message.createdAt | time }}</span>
+      </v-col>
     </template>
 
     <template v-else>
-      <span class="mr-2 caption">{{ message.createdAt | time }}</span>
-      <box-callout class="mr-3" :left="false">{{ message.text }}</box-callout>
+      <v-col cols="auto" align-self="end">
+        <span class="caption">{{ message.createdAt | time }}</span>
+      </v-col>
+
+      <!-- <template v-if="message.kind === 'IMAGE'">
+        <v-col cols="auto" align-self="center">
+          <button-file-download :origin-url="message.imageURL" />
+        </v-col>
+      </template> -->
+
+      <v-col cols="auto" align-self="end">
+        <template v-if="message.kind === 'TEXT'">
+          <box-callout :left="false">{{ message.text }}</box-callout>
+        </template>
+
+        <template v-if="message.kind === 'IMAGE'">
+          <box-callout :left="false">
+            <v-row no-gutters style="padding-bottom:6px;">
+              <v-col>
+                <chip-file-size :size="message.size" />
+              </v-col>
+            </v-row>
+            <v-img :src="message.imageURL" />
+            <v-row no-gutters justify="center" style="padding-top:6px;">
+              <v-col cols="auto">
+                <button-file-download :origin-url="message.imageURL" />
+              </v-col>
+            </v-row>
+          </box-callout>
+        </template>
+
+        <template v-else-if="message.kind === 'AUDIO'">
+          <box-callout :left="false">
+            <button-audio :src="message.audioURL" />
+          </box-callout>
+        </template>
+      </v-col>
     </template>
-  </div>
+  </v-row>
 </template>
 
 <script lang="ts">
 import { createComponent } from '@vue/composition-api'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
-import firebase from '@/plugins/firebase'
+import { mdiFileImage } from '@mdi/js'
 import BoxCallout from '@/components/atoms/SpeechBalloon.vue'
-
-export type Message = {
-  text: string
-  createdAt: firebase.firestore.FieldValue
-}
+import { MessageSet, FirestoreFieldValue } from '@/types/core'
+import ButtonAudio from '@/components/molecules/ButtonAudio.vue'
+import ButtonFileDownload from '@/components/molecules/ButtonFileDownload.vue'
+import ChipFileSize from '@/components/atoms/ChipFileSize.vue'
 
 export default createComponent({
   components: {
-    BoxCallout
+    BoxCallout,
+    ButtonAudio,
+    ButtonFileDownload,
+    ChipFileSize
   },
 
   filters: {
@@ -46,7 +95,7 @@ export default createComponent({
 
   props: {
     message: {
-      type: Object as () => Message,
+      type: Object as () => MessageSet & FirestoreFieldValue,
       required: true
     },
 
@@ -56,6 +105,8 @@ export default createComponent({
     }
   },
 
-  setup() {}
+  setup() {
+    return { mdiFileImage }
+  }
 })
 </script>
