682ada5f9915ed0492ed695e6e94cfc551319b2b
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-24 22:26:46 +0900

    :sparkles: (rtc.ts) Add store of rtc what manage rtc document id

---
+++ packages/client/src/store/rtc.ts
@@ -0,0 +1,29 @@
+import { Module, VuexModule, Mutation } from 'vuex-module-decorators'
+
+@Module({
+  name: 'rtc',
+  stateFactory: true,
+  namespaced: true
+})
+export default class Rtc extends VuexModule {
+  private _offerSDP = ''
+  private _id = ''
+
+  @Mutation
+  setId(id: string) {
+    this._id = id
+  }
+
+  @Mutation
+  setOfferSDP(offerSDP: string) {
+    this._offerSDP = offerSDP
+  }
+
+  get id() {
+    return this._id
+  }
+
+  get offerSDP() {
+    return this._offerSDP
+  }
+}
--- packages/client/src/utils/store-accessor.ts
+++ packages/client/src/utils/store-accessor.ts
@@ -1,12 +1,15 @@
+/* eslint-disable import/no-mutable-exports */
 import { Store } from 'vuex'
 import { getModule } from 'vuex-module-decorators'
 import Firestore from '@/store/firestore'
+import Rtc from '@/store/rtc'
 
-// eslint-disable-next-line import/no-mutable-exports
 let firestore: Firestore
+let rtc: Rtc
 
 function initializeStores(store: Store<any>): void {
   firestore = getModule(Firestore, store)
+  rtc = getModule(Rtc, store)
 }
 
-export { initializeStores, firestore }
+export { initializeStores, firestore, rtc }
