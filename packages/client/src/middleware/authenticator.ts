96e6991b53bfab02af7c4f87bccb6a35c45d5cad
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-26 03:16:39 +0900

    :sparkles: (authenticator.ts) Add middleware of authenticator

---
+++ packages/client/src/middleware/authenticator.ts
@@ -0,0 +1,15 @@
+import { Middleware } from '@nuxt/types'
+import { auth } from '@/plugins/firebase'
+import { user as userStore } from '@/store'
+
+const authenticator: Middleware = () => {
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      userStore.setUser(user)
+    } else {
+      userStore.removeUser()
+    }
+  })
+}
+
+export default authenticator
