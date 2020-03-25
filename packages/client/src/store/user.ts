786d1579597d70641537a233ca969a2978c18087
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-26 02:45:02 +0900

    :sparkles: (user.ts) Add store of user management

---
+++ packages/client/src/store/user.ts
@@ -0,0 +1,36 @@
+import { Module, VuexModule, Mutation } from 'vuex-module-decorators'
+import firebase from '@/plugins/firebase'
+
+@Module({
+  name: 'user',
+  stateFactory: true,
+  namespaced: true
+})
+export default class User extends VuexModule {
+  private _user: firebase.UserInfo | null = null
+
+  @Mutation
+  setUser(user: firebase.User) {
+    if (!user.providerData.length) return
+
+    this._user = user.providerData[0]
+  }
+
+  @Mutation
+  removeUser() {
+    this._user = null
+  }
+
+  get user() {
+    return this._user
+  }
+
+  get login(): boolean {
+    return !!this._user
+  }
+
+  get photoURL(): string {
+    if (!this._user || !this._user.photoURL) return ''
+    return this._user.photoURL
+  }
+}
--- packages/client/src/utils/store-accessor.ts
+++ packages/client/src/utils/store-accessor.ts
@@ -3,13 +3,16 @@ import { Store } from 'vuex'
 import { getModule } from 'vuex-module-decorators'
 import Firestore from '@/store/firestore'
 import Rtc from '@/store/rtc'
+import User from '@/store/user'
 
 let firestore: Firestore
 let rtc: Rtc
+let user: User
 
 function initializeStores(store: Store<any>): void {
   firestore = getModule(Firestore, store)
   rtc = getModule(Rtc, store)
+  user = getModule(User, store)
 }
 
-export { initializeStores, firestore, rtc }
+export { initializeStores, firestore, rtc, user }
