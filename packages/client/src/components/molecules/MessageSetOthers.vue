33b7f9703d84fb60d995a231d906a9ff22998a8d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-28 19:21:54 +0900

    :sparkles: (MessageSetOthers.vue) Add avatar and audio play button

--- packages/client/src/components/molecules/MessageSetOthers.vue
+++ packages/client/src/components/molecules/MessageSetOthers.vue
@@ -1,18 +1,13 @@
 <template>
   <div style="padding:12px;">
-    <v-row
-      justify="start"
-      no-gutters
-      class="flex-nowrap
-"
-    >
+    <v-row justify="start" no-gutters class="flex-nowrap">
       <v-col cols="auto" align-self="end">
         <v-avatar v-if="message.isAnonymous" color="grey" size="36">
           <v-icon>{{ mdiAccountCircle }}</v-icon>
         </v-avatar>
 
         <v-avatar v-else color="indigo" right size="36">
-          <span class="white--text headline">3</span>
+          <img :src="message.contributor.photoURL" />
         </v-avatar>
       </v-col>
 
@@ -22,7 +17,23 @@
         </template>
 
         <template v-else-if="message.kind === 'AUDIO'">
-          <box-callout left>{{ message.audioURL }}</box-callout>
+          <box-callout left>
+            <v-row no-gutters style="padding-bottom:6px;">
+              <v-col>
+                <chip-file-size :size="message.size" />
+              </v-col>
+            </v-row>
+            <v-row no-gutters justify="center">
+              <v-col cols="auto">
+                <button-audio :src="message.audioURL" />
+              </v-col>
+            </v-row>
+            <v-row no-gutters justify="center" style="padding-top:6px;">
+              <v-col cols="auto">
+                <button-file-download :origin-url="message.audioURL" />
+              </v-col>
+            </v-row>
+          </box-callout>
         </template>
 
         <template v-else-if="message.kind === 'IMAGE'">
@@ -47,7 +58,8 @@
     </v-row>
     <v-row no-gutters>
       <v-col cols="auto">
-        <user-name-anonymous />
+        <user-name-anonymous v-if="message.isAnonymous" />
+        <base-user-name v-else>{{ message.contributor.name }}</base-user-name>
       </v-col>
     </v-row>
   </div>
@@ -57,6 +69,7 @@
 import { mdiAccountCircle, mdiFileImage } from '@mdi/js'
 import { createComponent } from '@vue/composition-api'
 
+import BaseUserName from '@/components/atoms/BaseUserName.vue'
 import ChipFileSize from '@/components/atoms/ChipFileSize.vue'
 import MessageTimestamp from '@/components/atoms/MessageTimestamp.vue'
 import BoxCallout from '@/components/atoms/SpeechBalloon.vue'
@@ -71,7 +84,8 @@ export default createComponent({
     ButtonFileDownload,
     ChipFileSize,
     UserNameAnonymous,
-    MessageTimestamp
+    MessageTimestamp,
+    BaseUserName
   },
 
   props: {
