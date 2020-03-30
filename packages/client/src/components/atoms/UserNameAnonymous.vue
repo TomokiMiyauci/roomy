5fd504b66fb4f97a977954e6b53ef0be145a370d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-31 02:19:37 +0900

    :sparkles: (UserNameSnonymous.vue) Add atoms of anonymous nameplate

---
+++ packages/client/src/components/atoms/UserNameAnonymous.vue
@@ -0,0 +1,15 @@
+<template>
+  <base-user-name>Anonymous</base-user-name>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+import BaseUserName from '@/components/atoms/BaseUserName.vue'
+
+export default createComponent({
+  components: {
+    BaseUserName
+  }
+})
+</script>
