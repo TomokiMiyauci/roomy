bf816700f3ed58e9008512800b270dbc6dfbeb4d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-03-31 23:40:05 +0900

    :sparkles: (rooms.vue) Add rooms page

---
+++ packages/client/src/pages/rooms.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <the-rooms :rooms="rooms" />
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+import TheRooms from '@/components/organisms/TheRooms.vue'
+import { useFirestore } from '@/core/useFirestore'
+import { firestore } from '@/plugins/firebase'
+export default createComponent({
+  layout: 'app',
+
+  components: {
+    TheRooms
+  },
+
+  setup() {
+    const rooms = useFirestore(firestore.collection('rooms'))
+
+    return { rooms }
+  }
+})
+</script>
