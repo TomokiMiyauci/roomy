0812cfd5859b795c359ed325fff4473742692b4a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-04-06 00:13:52 +0900

    :label: (vue-shims.d.ts) Deal for main src and test

--- packages/client/src/types/vue-shims.d.ts
+++ packages/client/src/types/vue-shims.d.ts
@@ -1,4 +1,10 @@
+/* eslint-disable import/no-duplicates */
 import Vue from 'vue'
+
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
+}
 
 declare module 'vue/types/vue' {
   interface Vue {
