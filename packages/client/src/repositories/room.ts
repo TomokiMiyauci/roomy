43c853fc513ff9b683246048d3f95ba2f41bc2b2
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-04-12 21:51:46 +0900

    :sparkles: (room.ts) Add create private room feature

--- packages/client/src/repositories/room.ts
+++ packages/client/src/repositories/room.ts
@@ -1,19 +1,53 @@
 import { roomReference } from '@/core/useFirestoreReference'
+import firebase from '@/plugins/firebase'
 import { getUser } from '@/repositories/public'
 import { user } from '@/store'
+import { PrivateRoom, Room } from '@/types/core'
 import { getTimestamp, getTimestamps } from '@/utils/firestore'
+import { generateRandomB64 } from '@/utils/secret'
+
 export const createRoom = () => {
   const { collectionRef } = roomReference()
 
-  return collectionRef.value.add({
+  const room: Room = {
+    isPrivate: false,
+    members: [user.id],
     recent: {
       message: 'No messages',
       isAnonymous: true,
       ...getTimestamps()
     },
     name: 'New Room',
+    photoURL: '',
     ...getTimestamps()
-  })
+  }
+
+  return collectionRef.value.add(room)
+}
+
+export const createPrivateRoom = () => {
+  const { collectionRef } = roomReference()
+  const key = generateRandomB64()
+
+  const room: PrivateRoom = {
+    isPrivate: true,
+    key,
+    members: [user.id],
+    recent: {
+      shortMessage: 'No messages',
+      kind: 'TEXT',
+      author: {
+        isAnonymous: true
+      },
+      ...getTimestamps()
+    },
+    messageCount: 0,
+    name: 'New Room',
+    photoURL: '',
+    ...getTimestamps()
+  }
+
+  return collectionRef.value.add(room)
 }
 
 export const updateRoom = () => {
@@ -26,6 +60,14 @@ export const updateRoom = () => {
   })
 }
 
+export const getRoomKey = async (id: string) => {
+  const { collectionRef } = roomReference()
+  const documentData = await collectionRef.value.doc(id).get()
+  // return collectionRef.value.doc(id).get()
+  const data = documentData.data()
+  return data ? (data.key as string) : ''
+}
+
 export const updateRecent = (message: string) => {
   const { documentRef } = roomReference()
 
@@ -35,5 +77,13 @@ export const updateRecent = (message: string) => {
       ...getUser(user.id),
       ...getTimestamp('updatedAt')
     }
+  })
+}
+
+export const joinRoom = (id: string, userId: string) => {
+  const { collectionRef } = roomReference()
+
+  return collectionRef.value.doc(id).update({
+    members: firebase.firestore.FieldValue.arrayUnion(userId)
   })
 }
