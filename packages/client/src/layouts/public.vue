43fb59ff88f9b5e866c7b919646ec08b97a0c975
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-04-13 23:35:51 +0900

    :sparkles: (public.vue) Add layout for public room

---
+++ packages/client/src/layouts/public.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app>
+    <the-navigation-drawer-left />
+    <the-navigation-drawer-right :rooms="rooms" />
+    <the-app-bar-private
+      :login="login"
+      :photo-u-r-l="photoURL"
+      @click="$nuxt.$emit('close')"
+    />
+
+    <v-content>
+      <transition name="fade">
+        <nuxt />
+      </transition>
+    </v-content>
+
+    <client-only>
+      <the-bottom-navigation v-if="$vuetify.breakpoint.mdAndDown" />
+    </client-only>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+import { useFirestore } from '@/core/useFirestore'
+import { roomReference } from '@/core/useFirestoreReference'
+import { user } from '@/store'
+
+export default defineComponent({
+  components: {
+    TheAppBarPrivate: () =>
+      import('@/components/organisms/TheAppBarPrivate.vue'),
+    TheNavigationDrawerLeft: () =>
+      import('@/components/organisms/TheNavigationDrawerLeft.vue'),
+    TheNavigationDrawerRight: () =>
+      import('@/components/organisms/TheNavigationDrawerRight.vue'),
+    TheBottomNavigation: () =>
+      import('@/components/organisms/TheBottomNavigation.vue')
+  },
+
+  setup() {
+    const { collectionRef } = roomReference()
+    const rooms = useFirestore(
+      collectionRef.value
+        .where('isPrivate', '==', false)
+        .orderBy('recent.updatedAt', 'desc')
+    )
+
+    return { photoURL: user.photoURL, login: user.login, rooms }
+  }
+})
+</script>
+
+<style lang="scss">
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 2s;
+}
+</style>
