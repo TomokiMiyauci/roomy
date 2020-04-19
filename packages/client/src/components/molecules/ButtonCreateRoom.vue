10ce036f812b919e658a119673d7145cbbef288f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-04-19 12:27:09 +0900

    :sparkles: (ButtonCreateRoom.vue) Add button fir create room trigger

---
+++ packages/client/src/components/molecules/ButtonCreateRoom.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-menu v-bind="$attrs" open-on-hover open-on-click>
+    <template #activator="{ on: menu }">
+      <v-tooltip bottom>
+        <template #activator="{ on }">
+          <v-btn
+            v-bind="$attrs"
+            fab
+            :color="login ? 'primary' : 'grey'"
+            v-on="menu"
+            @click="login ? $emit('click') : ''"
+            @mouseenter="on.mouseenter"
+            @mouseleave="on.mouseleave"
+          >
+            <v-icon>{{ mdiCommentPlus }}</v-icon></v-btn
+          >
+        </template>
+        <span>Create Room</span>
+      </v-tooltip>
+    </template>
+    <card-signin-prompt v-if="!login"></card-signin-prompt>
+  </v-menu>
+</template>
+
+<script lang="ts">
+import { mdiCommentPlus } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  components: {
+    CardSigninPrompt: () =>
+      import('@/components/molecules/CardSigninPrompt.vue')
+  },
+
+  props: {
+    login: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  setup() {
+    return { mdiCommentPlus }
+  }
+})
+</script>
