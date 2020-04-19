78f63650dec916f3918b4b122883233eafa592dc
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-04-19 13:07:13 +0900

    :sparkles: (middleware/*) Add room keeper what manage non signin user

--- packages/client/src/middleware/gateKeeper.ts
+++ packages/client/src/middleware/gateKeeper.ts
@@ -1,28 +1,10 @@
 import { Middleware } from '@nuxt/types'
 
-import { firestore } from '@/plugins/firebase'
-const existsRoom = async (id: string) => {
-  const doc = await firestore
-    .collection('rooms')
-    .doc(id)
-    .get()
+import { user } from '@/store'
 
-  return doc
-}
-
-const gateKeeper: Middleware = async ({ route, redirect }) => {
-  const { roomId, key } = route.query
-  if (!roomId || typeof roomId === 'object' || !key) {
-    redirect('/')
-    return
-  }
-
-  const documentData = await existsRoom(roomId)
-
-  const data = documentData.data()
-
-  if (!documentData.exists || !data || data.key !== key) {
-    redirect('/')
+const gateKeeper: Middleware = ({ redirect }) => {
+  if (!user.login) {
+    redirect('/public')
   }
 }
 
