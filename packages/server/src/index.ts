4fbf6a5a9d7df3f874128d37c44a51e82792d5fd
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-04-19 13:32:53 +0900

    :sparkles: (Add to rewrite message to recent)

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,40 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+// import nuxtServer from './nuxt-server'
+import { RecentMessage, Message } from "../../share/core";
+// exports.nuxtServer = functions
+//   .runWith({ memory: '2GB' })
+//   .https.onRequest(nuxtServer)
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+
+exports.hello = functions.runWith({memory: '2GB'}).firestore.document('rooms/{roomId}/messages/{messageId}').onCreate(async (snapshot)=> {
+  // console.log(snapshot, context);
+
+  const { size } = await snapshot.ref.parent.get()
+  const data  = snapshot.data() as Message | undefined
+
+  if( !data || !snapshot.ref.parent.parent) return
+
+  if(!data.author.isAnonymous) {
+    return
+  }
+
+  const {kind, createdAt, updatedAt } = data
+  const shortMessage = data.kind === 'TEXT'? data.text: 'Something posted'
+
+
+  const recentMessage: RecentMessage ={
+    createdAt,
+    updatedAt,
+    kind,
+    shortMessage,
+    author: {
+      isAnonymous: true
+    }
+  }
+
+  return snapshot.ref.parent.parent.update({
+    messageCount: size,
+    recent: recentMessage
+  })
+
+})
