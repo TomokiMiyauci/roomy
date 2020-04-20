67c35e1528f6857c74eaf70b95d2c4a4b542ab22
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-04-20 19:38:31 +0900

    :bug: (index.vue) Debug process env

--- packages/client/src/pages/index.vue
+++ packages/client/src/pages/index.vue
@@ -1,12 +1,13 @@
 <template>
-  <div></div>
+  <div>{{ hello }}</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 export default defineComponent({
-  setup(_, { root }) {
-    root.$router.push('/public')
+  setup(_) {
+    // root.$router.push('/public')
+    return { hello: process.env.PROJECT_ID }
   }
 })
 </script>
