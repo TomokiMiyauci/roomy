33a055ce30c3394ac76bc9fd71aff9de85a69f3c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-04-30 21:52:22 +0900

    :wrench: (nuxt.config.ts) Add env for client use

--- packages/client/nuxt.config.ts
+++ packages/client/nuxt.config.ts
@@ -14,7 +14,16 @@ const config: Configuration = {
     baseUrl:
       process.env.NODE_ENV === 'development'
         ? 'http://localhost:3000'
-        : process.env.AUTH_DOMAIN!
+        : process.env.AUTH_DOMAIN!,
+
+    API_KEY: process.env.API_KEY!,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN!,
+    DATABASE_URL: process.env.DATABASE_URL!,
+    PROJECT_ID: process.env.PROJECT_ID!,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET!,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID!,
+    APP_ID: process.env.APP_ID!,
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID!
   },
 
   router: {
