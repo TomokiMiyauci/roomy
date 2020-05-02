a062055777665e5874bcfd28fdff6c02a3255920
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-02 11:36:35 +0900

    :sparkles: (axe.ts) Localize axe

--- packages/client/src/plugins/axe.ts
+++ packages/client/src/plugins/axe.ts
@@ -2,7 +2,12 @@ import Vue from 'vue'
 
 if (process.env.NODE_ENV !== 'production') {
   const VueAxe = require('vue-axe')
+  const AXE_LOCALE_JA = require('axe-core/locales/ja.json') // locale setting
+
   Vue.use(VueAxe, {
+    config: {
+      locale: AXE_LOCALE_JA
+    },
     clearConsoleOnUpdate: false
   })
 }
