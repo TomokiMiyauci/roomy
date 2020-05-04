745028193aed0a93b5474080e5847921af6d3052
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-04 19:08:03 +0900

    :sparkles: (ButtonQrcodeReader.vue) Add qrcode
    reader button

---
+++ packages/client/src/components/molecules/ButtonQrcodeReader.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn v-on="$listeners" icon color="secondary">
+    <v-icon>{{ mdiQrcodeScan }}</v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { mdiQrcodeScan } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    return { mdiQrcodeScan }
+  }
+})
+</script>
