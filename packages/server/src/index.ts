dc8e34da11cb3183cdb2578009c9826172c461cd
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-04 09:49:14 +0900

    :sparkles: (index.ts) Add short message register

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,11 @@
 import * as functions from 'firebase-functions'
 // import nuxtServer from './nuxt-server'
-import { RecentMessage, Message } from "../../share/core";
+import { RecentMessage, Message, ShortMessage } from "~types/core";
 // exports.nuxtServer = functions
 //   .runWith({ memory: '2GB' })
 //   .https.onRequest(nuxtServer)
 
-
-exports.hello = functions.runWith({memory: '2GB'}).firestore.document('rooms/{roomId}/messages/{messageId}').onCreate(async (snapshot)=> {
+exports.hello = functions.runWith({memory: '2GB'}).region('asia-northeast1').firestore.document('rooms/{roomId}/messages/{messageId}').onCreate(async (snapshot)=> {
   // console.log(snapshot, context);
 
   const { size } = await snapshot.ref.parent.get()
@@ -14,22 +13,34 @@ exports.hello = functions.runWith({memory: '2GB'}).firestore.document('rooms/{ro
 
   if( !data || !snapshot.ref.parent.parent) return
 
-  if(!data.author.isAnonymous) {
-    return
+
+  const {kind, createdAt, updatedAt,author } = data
+
+  let shortMessage: ShortMessage = 'No messages'
+
+  switch(data.kind){
+    case 'TEXT':{
+      shortMessage = data.text
+      break
+    }
+
+    case 'AUDIO': {
+      shortMessage = 'Audio posted'
+      break
+    }
+
+    case 'IMAGE': {
+      shortMessage = 'Image posted'
+      break
+    }
   }
-
-  const {kind, createdAt, updatedAt } = data
-  const shortMessage = data.kind === 'TEXT'? data.text: 'Something posted'
-
 
   const recentMessage: RecentMessage ={
     createdAt,
     updatedAt,
     kind,
     shortMessage,
-    author: {
-      isAnonymous: true
-    }
+    author
   }
 
   return snapshot.ref.parent.parent.update({
