29b7f7f8c2f5b40e5ccfd357ff7e36668017f68a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-12 23:30:08 +0900

    :sparkles: Remove unused and add trigger for create user

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,51 +1,80 @@
 import * as functions from 'firebase-functions'
-// import nuxtServer from './nuxt-server'
-import { RecentMessage, Message, ShortMessage } from "~types/core";
-// exports.nuxtServer = functions
-//   .runWith({ memory: '2GB' })
-//   .https.onRequest(nuxtServer)
+import * as admin from 'firebase-admin'
+import { RecentMessage, Message, ShortMessage } from '~types/core'
 
-exports.hello = functions.runWith({memory: '2GB'}).region('asia-northeast1').firestore.document('rooms/{roomId}/messages/{messageId}').onCreate(async (snapshot)=> {
-  // console.log(snapshot, context);
+admin.initializeApp()
+const firestore = admin.firestore()
 
-  const { size } = await snapshot.ref.parent.get()
-  const data  = snapshot.data() as Message | undefined
+type Profile = {
+  displayName: firebase.User['displayName']
+  photoURL: firebase.User['photoURL']
+}
 
-  if( !data || !snapshot.ref.parent.parent) return
+export const onCreateUser = functions
+  .region('asia-northeast1')
+  .auth.user()
+  .onCreate(async (user) => {
+    await admin.auth().setCustomUserClaims(user.uid, {
+      suspend: false
+    })
 
-
-  const {kind, createdAt, updatedAt,author } = data
-
-  let shortMessage: ShortMessage = 'No messages'
-
-  switch(data.kind){
-    case 'TEXT':{
-      shortMessage = data.text
-      break
+    const { displayName: newDisplayName, photoURL: newPhotoURL } = user
+    const displayName = newDisplayName ? newDisplayName : null
+    const photoURL = newPhotoURL ? newPhotoURL : null
+    const profile: Profile = {
+      displayName,
+      photoURL
     }
 
-    case 'AUDIO': {
-      shortMessage = 'Audio posted'
-      break
-    }
-
-    case 'IMAGE': {
-      shortMessage = 'Image posted'
-      break
-    }
-  }
-
-  const recentMessage: RecentMessage ={
-    createdAt,
-    updatedAt,
-    kind,
-    shortMessage,
-    author
-  }
-
-  return snapshot.ref.parent.parent.update({
-    messageCount: size,
-    recent: recentMessage
+    await firestore
+      .collection('profiles')
+      .doc(user.uid)
+      .set(profile)
   })
 
-})
+exports.hello = functions
+  .runWith({ memory: '2GB' })
+  .region('asia-northeast1')
+  .firestore.document('rooms/{roomId}/messages/{messageId}')
+  .onCreate(async (snapshot) => {
+    // console.log(snapshot, context);
+
+    const { size } = await snapshot.ref.parent.get()
+    const data = snapshot.data() as Message | undefined
+
+    if (!data || !snapshot.ref.parent.parent) return
+
+    const { kind, createdAt, updatedAt, author } = data
+
+    let shortMessage: ShortMessage = 'No messages'
+
+    switch (data.kind) {
+      case 'TEXT': {
+        shortMessage = data.text
+        break
+      }
+
+      case 'AUDIO': {
+        shortMessage = 'Audio posted'
+        break
+      }
+
+      case 'IMAGE': {
+        shortMessage = 'Image posted'
+        break
+      }
+    }
+
+    const recentMessage: RecentMessage = {
+      createdAt,
+      updatedAt,
+      kind,
+      shortMessage,
+      author
+    }
+
+    return snapshot.ref.parent.parent.update({
+      messageCount: size,
+      recent: recentMessage
+    })
+  })
