03d40cf6ca3c619a9ab23a02e01ce3c34528b1c6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-16 04:43:10 +0900

    :white_check_mark: (firestore.spec.ts) Add firestore rules test

---
+++ packages/server/test/firestore.spec.ts
@@ -0,0 +1,69 @@
+import * as firebase from '@firebase/testing'
+import fs from 'fs'
+import { publicRoom } from '../../share/__mock__/'
+
+const PROJECT_ID = 'test'
+
+describe('firestore', () => {
+  //実行前に一度だけ実行（初期化）
+  beforeAll(async () => {
+    await firebase.loadFirestoreRules({
+      projectId: PROJECT_ID,
+      rules: fs.readFileSync('../../firebase/firestore.rules', 'utf8')
+    })
+  })
+
+  //ブロックが終わるたび実行
+  afterEach(async () => {
+    await firebase.clearFirestoreData({ projectId: PROJECT_ID }) //データリセット
+  })
+
+  //終わった後に一度だけ実行
+  afterAll(async () => {
+    await Promise.all(
+      firebase.apps().map((app) => app.delete()) //生成したアプリを削除
+    )
+  })
+
+  //条件(projectIdとauth情報）をの指定を関数化
+  //auth : {uid:'alice'}
+  //auth : {uid:'alice', admin:true} admin
+  //auth : null 未認証
+  function authedApp(auth: any): firebase.firestore.Firestore {
+    return firebase
+      .initializeTestApp({
+        projectId: PROJECT_ID,
+        auth: auth
+      })
+      .firestore()
+  }
+
+  describe('public-rooms', () => {
+    it('should read when authed', async () => {
+      const firestore = authedApp({ uid: 'admin', name: 'admin' })
+      const publicRooms = firestore.collection('public-rooms')
+
+      await firebase.assertSucceeds(publicRooms.get())
+    })
+
+    it('should write when authed', async () => {
+      const firestore = authedApp({
+        uid: 'authenticated',
+        name: 'authenticated'
+      })
+      const publicRooms = firestore.collection('public-rooms')
+
+      await firebase.assertSucceeds(publicRooms.add(publicRoom))
+    })
+
+    it('should not get', async () => {
+      const firestore = authedApp({
+        uid: 'authenticated',
+        name: 'authenticated'
+      })
+
+      const publicRooms = firestore.collection('public-rooms').doc('something')
+      await firebase.assertFails(publicRooms.get())
+    })
+  })
+})
