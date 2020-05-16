cffc76dd24733f88de28003790831e6564c80ada
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-17 03:13:59 +0900

    :sparkles: (user.ts) Add subscriber for view history in store

--- packages/client/src/store/user.ts
+++ packages/client/src/store/user.ts
@@ -1,6 +1,9 @@
 import { Action, Module, Mutation, VuexModule } from 'vuex-module-decorators'
 
+import { isDef } from '@/core/useFirestore'
+import { userRef } from '@/core/useFirestoreReference'
 import firebase from '@/plugins/firebase'
+import { PublicRoom } from '~types/core'
 
 @Module({
   name: 'user',
@@ -13,6 +16,65 @@ export default class User extends VuexModule {
   private _photoURL: firebase.User['photoURL'] = null
   private _successfulSignIn: boolean = false
   private _providerData: firebase.UserInfo | null = null
+  private _viewHistories: any = []
+
+  @Mutation
+  setViewHistories(viewHistories: any) {
+    this._viewHistories = viewHistories
+  }
+
+  @Action
+  subscribe() {
+    const { documentRef } = userRef()
+
+    documentRef.value
+      .collection('view-histories')
+      .orderBy('updatedAt', 'desc')
+      .onSnapshot(async (snapshot) => {
+        const viewHistories = await Promise.all(
+          snapshot.docs
+            .map(async (value) => {
+              const data = value.data()
+              if (!data) return data
+
+              const { ref, messageDiff } = data
+
+              const doc = await ref.get()
+              const {
+                name,
+                photoURL,
+                createdAt,
+                messageCount,
+                recent
+              } = doc.data()! as PublicRoom
+              if (!recent.author.isAnonymous) delete recent.author.ref
+              const publicRoom = {
+                name,
+                photoURL,
+                createdAt,
+                recent,
+                messageCount,
+                messageDiff,
+                updatedAt: data.updatedAt
+              }
+
+              Object.defineProperty(publicRoom, 'id', {
+                value: value.id.toString(),
+                writable: false
+              })
+
+              return publicRoom
+            })
+            .filter(isDef)
+        )
+
+        this.setViewHistories(viewHistories)
+      })
+  }
+
+  get viewHistories() {
+    return this._viewHistories
+  }
 
   @Action
   setUser(user: firebase.User): void {
