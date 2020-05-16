77ef7b45de3028b42188d31e278ae9512e92be82
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-17 03:10:46 +0900

    :sparkles: (users.ts) Add repository of view history

---
+++ packages/client/src/repositories/users.ts
@@ -0,0 +1,41 @@
+import { publicRoomRef, userRef } from '@/core/useFirestoreReference'
+import firebase from '@/plugins/firebase'
+
+export const enterRoom = (uid: string) => {
+  const { documentRef } = userRef()
+  const { documentRef: prRef } = publicRoomRef()
+
+  return documentRef.value
+    .collection('view-histories')
+    .doc(uid)
+    .set(
+      {
+        updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+        messageDiff: 0,
+        ref: prRef.value
+      },
+      {
+        merge: true
+      }
+    )
+}
+
+// export const get = async () => {
+//   const ref = await firestore
+//     .collectionGroup('view-histories')
+//     .where(
+//       'ref',
+//       '==',
+//       firestore.collection('public-rooms').doc('EdG8q88GcVAP2nR6GKIH')
+//     )
+//     .get()
+
+//   console.log(ref)
+//   console.log(ref.docs, ref.query)
+
+//   ref.forEach(async (doc) => {
+//     await doc.ref.update({
+//       messageDiff: firebase.firestore.FieldValue.increment(1)
+//     })
+//   })
+// }
