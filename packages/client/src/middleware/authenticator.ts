d9b32a7109a757f4c813ed1518ea157f9be75811
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-18 01:54:30 +0900

    :sparkles: (authenticator.ts) Add server side token vetiry

--- packages/client/src/middleware/authenticator.ts
+++ packages/client/src/middleware/authenticator.ts
@@ -1,18 +1,31 @@
 import { Middleware } from '@nuxt/types'
 
 import { auth } from '@/plugins/firebase'
+import admin from '@/plugins/firebase-admin'
 import { user as userStore } from '@/store'
 
-const authenticator: Middleware = () => {
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      console.log(user)
+const authenticator: Middleware = async (ctx) => {
+  if (process.server && ctx.req.headers.cookie) {
+    const result = await admin
+      .auth()
+      .verifyIdToken(ctx.req.headers.cookie)
+      .catch((e: any) => {
+        console.log(e)
+        document.cookie = ''
+      })
+    if (!result) return
+    const { name: displayName, picture: photoURL, user_id: uid } = result
 
-      userStore.setUser(user)
-    } else {
-      userStore.removeUser()
-    }
-  })
+    userStore.setUser({ displayName, photoURL, uid })
+  } else if (process.client) {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        document.cookie = await user.getIdToken()
+
+        userStore.setUser(user)
+      }
+    })
+  }
 }
 
 export default authenticator
