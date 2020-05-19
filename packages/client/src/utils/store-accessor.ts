732ee931bf5c5c64a2977da93d22a8b210d3c3b5
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-20 01:09:03 +0900

    :sparkles: (store-accessor.ts) Add user store

--- packages/client/src/utils/store-accessor.ts
+++ packages/client/src/utils/store-accessor.ts
@@ -8,12 +8,16 @@ import Public from '@/store/publicRoom'
 import Reference from '@/store/reference'
 import Rtc from '@/store/rtc'
 import User from '@/store/user'
+import FavoriteRoom from '@/store/user/favorite-room'
+import ViewHistory from '@/store/user/view-history'
 
 let reference: Reference
 let rtc: Rtc
 let user: User
 let privateRoom: Private
 let publicRoom: Public
+let viewHistory: ViewHistory
+let favoriteRoom: FavoriteRoom
 
 function initializeStores(store: Store<any>): void {
   reference = getModule(Reference, store)
@@ -21,6 +25,17 @@ function initializeStores(store: Store<any>): void {
   user = getModule(User, store)
   privateRoom = getModule(Private, store)
   publicRoom = getModule(Public, store)
+  viewHistory = getModule(ViewHistory, store)
+  favoriteRoom = getModule(FavoriteRoom, store)
 }
 
-export { initializeStores, reference, rtc, user, privateRoom, publicRoom }
+export {
+  initializeStores,
+  reference,
+  rtc,
+  user,
+  privateRoom,
+  publicRoom,
+  viewHistory,
+  favoriteRoom
+}
