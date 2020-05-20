daa08ce11ec314f157fe6abbd60a43b9d8a5466b
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-20 22:18:49 +0900

    :sparkles: (InputTextareaMessage.vue) Isolate textarea for message

---
+++ packages/client/src/components/atoms/InputTextareaMessage.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-textarea
+    :rules="[(v) => !!v || 'required']"
+    :prepend-inner-icon="mdiChatProcessing"
+    :clear-icon="mdiCloseCircle"
+    v-on="$listeners"
+    :value="value"
+    type="text"
+    clearable
+    auto-grow
+    solo
+    dense
+    rows="1"
+    hide-details
+    label="Enter message"
+    rounded
+    light
+    auto-focus
+  ></v-textarea>
+</template>
+
+<script lang="ts">
+import { mdiChatProcessing, mdiCloseCircle } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+
+  setup() {
+    return { mdiChatProcessing, mdiCloseCircle }
+  }
+})
+</script>
