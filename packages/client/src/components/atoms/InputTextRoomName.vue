d7f33762df1d2f5570913641f32cea2ef1e521f5
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-21 23:45:33 +0900

    :sparkles: (InputTextRoomName.vue) Add room name input element

---
+++ packages/client/src/components/atoms/InputTextRoomName.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-text-field
+    :clear-icon="mdiCloseCircle"
+    :prepend-inner-icon="mdiSemanticWeb"
+    :rules="[required]"
+    :value="value"
+    v-on="$listeners"
+    placeholder="Enter room name"
+    clearable
+    outlined
+    hint="e.g.: Roomy Official"
+    label="Room Name"
+  ></v-text-field>
+</template>
+
+<script lang="ts">
+import { mdiCloseCircle, mdiSemanticWeb } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+
+import { required } from '@/utils/validator'
+export default defineComponent({
+  props: {
+    value: {
+      type: String || Number,
+      default: ''
+    }
+  },
+
+  setup() {
+    return { required, mdiCloseCircle, mdiSemanticWeb }
+  }
+})
+</script>
