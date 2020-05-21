62f6af0a0b93799c34cb850b2662c803545f9dc0
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-22 03:01:44 +0900

    :recycle: (index.ts) Separete functions from main

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,9 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
-import { RecentMessage, Message, ShortMessage } from '~types/core'
 admin.initializeApp()
 
 import { createPublicRoom } from './publicRoom/'
+import { createPublicRoomMessage } from './publicRoom/message'
 
 const firestore = admin.firestore()
 
@@ -43,80 +43,4 @@ export const onCreateUser = functions
     return batch.commit()
   })
 
-exports.onCreatePublicRoomMessage = functions
-  .region('asia-northeast1')
-  .firestore.document('public-rooms/{roomId}/messages/{messageId}')
-  .onCreate(async (snapshot, { params }) => {
-    const data = snapshot.data() as Message | undefined
-
-    if (!data || !snapshot.ref.parent.parent) return
-
-    const { kind, createdAt, updatedAt, author } = data
-
-    let shortMessage: ShortMessage = 'No messages'
-
-    switch (data.kind) {
-      case 'TEXT': {
-        shortMessage = data.text
-        break
-      }
-
-      case 'AUDIO': {
-        shortMessage = 'Audio posted'
-        break
-      }
-
-      case 'IMAGE': {
-        shortMessage = 'Image posted'
-        break
-      }
-    }
-
-    const recentMessage: RecentMessage = {
-      createdAt,
-      updatedAt,
-      kind,
-      shortMessage,
-      author
-    }
-
-    const batch = firestore.batch()
-    const increment = admin.firestore.FieldValue.increment(1)
-    const uid = !data.author.isAnonymous ? data.author.ref.id : ''
-
-    batch.update(snapshot.ref.parent.parent, {
-      messageCount: increment,
-      recent: recentMessage
-    })
-
-    if (uid) {
-      batch.set(
-        firestore
-          .collection('users')
-          .doc(uid)
-          .collection('interested-rooms')
-          .doc(params.roomId),
-        { updatedAt: admin.firestore.FieldValue.serverTimestamp() },
-        { merge: true }
-      )
-    }
-
-    const ref = await firestore
-      .collectionGroup('view-histories')
-      .where('ref', '==', snapshot.ref.parent.parent)
-      .get()
-
-    ref.forEach((doc) => {
-      if (
-        data.author.isAnonymous ||
-        !doc.ref.parent.parent ||
-        data.author.ref.id !== doc.ref.parent.parent.id
-      ) {
-        batch.update(doc.ref, {
-          messageDiff: increment
-        })
-      }
-    })
-
-    return batch.commit()
-  })
+export const onCreatePublicRoomMessage = createPublicRoomMessage
