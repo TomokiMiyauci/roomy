b36ea5fcb12276d663ac8e760b9ab00b69267fe9
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-23 03:34:47 +0900

    :sparkles: (InputTextSearch.vue) Add input element for search

---
+++ packages/client/src/components/atoms/InputTextSearch.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-text-field
+    :prepend-inner-icon="mdiMagnify"
+    :clear-icon="mdiCloseCircle"
+    :value="value"
+    v-on="$listeners"
+    hide-details
+    dense
+    solo
+    rounded
+    clearable
+    placeholder="Enter keywords"
+  />
+</template>
+
+<script lang="ts">
+import { mdiCloseCircle, mdiMagnify } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+export default defineComponent({
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+
+  setup() {
+    return { mdiMagnify, mdiCloseCircle }
+  }
+})
+</script>
