291b9615cf35ce479a30d3378a7abc6f91fb444b
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-23 03:35:41 +0900

    :sparkles: Add base button and chip

---
+++ packages/client/src/components/molecules/ButtonQrcode.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn
+    v-on="$listeners"
+    v-bind="$attrs"
+    aria-label="qrcode"
+    icon
+    color="secondary"
+  >
+    <v-icon color="grey lighten-1">{{ mdiQrcode }}</v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { mdiQrcode } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    return { mdiQrcode }
+  }
+})
+</script>
---
+++ packages/client/src/components/molecules/ChipTag.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-chip
+    v-on="$listeners"
+    v-bind="$attrs"
+    :close="enableClose"
+    :color="color"
+    text-color="white"
+    label
+  >
+    <v-icon left>{{ mdiTagText }}</v-icon>
+
+    <span class="pr-2 pl-2 title">
+      {{ text }}
+    </span>
+  </v-chip>
+</template>
+
+<script lang="ts">
+import { mdiTagText } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+
+    enableClose: {
+      type: Boolean,
+      default: false
+    },
+
+    color: {
+      type: String,
+      default: 'primary'
+    }
+  },
+
+  setup() {
+    return { mdiTagText }
+  }
+})
+</script>
