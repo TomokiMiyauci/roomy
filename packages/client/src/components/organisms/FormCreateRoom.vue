a49e0d5116e9bae70938679368713b131a624986
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-22 13:00:33 +0900

    :sparkles: (FormCreateRoom.vue) Add registering tag and improve ui

--- packages/client/src/components/organisms/FormCreateRoom.vue
+++ packages/client/src/components/organisms/FormCreateRoom.vue
@@ -27,16 +27,7 @@
         <v-window v-model="stepWindow">
           <v-window-item :value="1">
             <v-card-text>
-              <v-text-field
-                v-model="name"
-                :clear-icon="mdiCloseCircle"
-                :prepend-inner-icon="mdiSemanticWeb"
-                :rules="[(v) => !!v || 'This field is required']"
-                clearable
-                outlined
-                hint="Ex. Roomy Official"
-                label="Room Name"
-              ></v-text-field>
+              <input-text-room-name v-model="name" />
             </v-card-text>
           </v-window-item>
 
@@ -54,6 +45,12 @@
           </v-window-item>
 
           <v-window-item :value="3">
+            <v-card-text>
+              <combobox-tabs v-model="tags" />
+            </v-card-text>
+          </v-window-item>
+
+          <v-window-item :value="4">
             <div
               :style="{ height: $vuetify.breakpoint.mdAndDown ? '40vh' : '' }"
               class="pa-4 text-center"
@@ -93,69 +90,69 @@
         </v-window>
       </v-form>
 
-      <v-card-text>
-        <v-subheader>
-          <v-icon left>{{ mdiFormatListText }}</v-icon>
-          Preview
-        </v-subheader>
-        <v-list :max-width="$vuetify.breakpoint.mdAndDown ? '' : '360px'">
-          <v-list-item>
-            <v-list-item-avatar
+      <v-subheader>
+        <v-icon left>{{ mdiFormatListText }}</v-icon>
+        <span class="pr-3">Preview</span>
+        <v-divider />
+      </v-subheader>
+
+      <v-list :max-width="$vuetify.breakpoint.mdAndDown ? '' : '360px'">
+        <v-list-item>
+          <v-list-item-avatar
+            :style="{
+              border:
+                stepWindow === 2
+                  ? `2px solid ${$vuetify.theme.themes.light.primary}`
+                  : ''
+            }"
+            tile
+          >
+            <v-img v-if="photoURL" :src="photoURL" />
+            <v-icon v-else-if="!photoURL && stepWindow === 2" color="primary">{{
+              mdiNewBox
+            }}</v-icon>
+            <v-skeleton-loader v-else tile type="avatar" />
+          </v-list-item-avatar>
+
+          <v-list-item-content class="pt-1 pb-1">
+            <v-list-item-title
               :style="{
-                border:
-                  stepWindow === 2
-                    ? `2px solid ${$vuetify.theme.themes.light.primary}`
-                    : ''
+                color:
+                  stepWindow === 1 ? $vuetify.theme.themes.light.primary : ''
               }"
-              tile
             >
-              <v-img v-if="image" :src="image" />
-              <v-icon v-else-if="!image && stepWindow === 2" color="primary">{{
-                mdiNewBox
-              }}</v-icon>
-              <v-skeleton-loader v-else tile type="avatar" />
-            </v-list-item-avatar>
+              <span v-show="name">{{ name }}</span>
+              <v-skeleton-loader v-show="!name" type="text" />
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              <v-avatar color="grey" size="24">
+                <v-icon>{{ mdiAccountCircle }}</v-icon>
+              </v-avatar>
+            </v-list-item-subtitle>
 
-            <v-list-item-content class="pt-1 pb-1">
-              <v-list-item-title
-                :style="{
-                  color:
-                    stepWindow === 1 ? $vuetify.theme.themes.light.primary : ''
-                }"
-              >
-                <span v-show="name">{{ name }}</span>
-                <v-skeleton-loader v-show="!name" type="text" />
-              </v-list-item-title>
-              <v-list-item-subtitle>
-                <v-avatar color="grey" size="24">
-                  <v-icon>{{ mdiAccountCircle }}</v-icon>
-                </v-avatar>
-              </v-list-item-subtitle>
+            <div class="grey-darken-1--text caption">
+              <v-icon small left>{{ mdiChatProcessing }}</v-icon>
+              0
+            </div>
+          </v-list-item-content>
+          <v-list-item-action>
+            <span
+              class="grey-darken-1--text caption"
+              style="position:absolute;bottom:0;"
+              >Monday</span
+            >
+            <v-btn icon>
+              <v-icon color="grey lighten-1">{{ mdiQrcode }}</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
 
-              <div class="grey-darken-1--text caption">
-                <v-icon small left>{{ mdiChatProcessing }}</v-icon>
-                0
-              </div>
-            </v-list-item-content>
-            <v-list-item-action>
-              <span
-                class="grey-darken-1--text caption"
-                style="position:absolute;bottom:0;"
-                >Monday</span
-              >
-              <v-btn icon>
-                <v-icon color="grey lighten-1">{{ mdiQrcode }}</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </v-list-item>
-        </v-list>
-      </v-card-text>
-
-      <v-divider></v-divider>
+      <v-divider />
 
       <v-card-actions>
         <v-btn
-          :disabled="stepWindow === 1 || stepWindow === 3"
+          :disabled="stepWindow === 1 || stepWindow === 4"
           @click="stepWindow--"
           text
         >
@@ -163,7 +160,7 @@
         </v-btn>
         <v-spacer></v-spacer>
         <v-btn
-          :disabled="stepWindow === 3 || !valid"
+          :disabled="stepWindow === 4 || !valid"
           @click="onNext"
           color="primary"
           depressed
@@ -181,14 +178,13 @@ import {
   mdiChatProcessing,
   mdiCheckCircle,
   mdiClose,
-  mdiCloseCircle,
   mdiCogs,
   mdiFormatListText,
   mdiImageEdit,
   mdiNewBox,
   mdiQrcode,
-  mdiSemanticWeb,
-  mdiSquareEditOutline
+  mdiSquareEditOutline,
+  mdiTagPlus
 } from '@mdi/js'
 import {
   computed,
@@ -198,24 +194,28 @@ import {
   toRefs
 } from '@vue/composition-api'
 
+import InputTextRoomName from '@/components/atoms/InputTextRoomName.vue'
+import ComboboxTabs from '@/components/molecules/ComboboxTabs.vue'
 import { blobToDataURL as b } from '@/core/useFileDialog'
 import { useStep } from '@/core/useStep'
 import { wait } from '@/core/useTime'
 import { createRoom } from '@/repositories/publicRoom'
-// import { createPublicRoom } from '@/repositories/room'
 import { RoomOptions } from '~types/core'
 
 export default defineComponent({
   components: {
     FileDropper: () => import('@/components/molecules/FileDropper.vue'),
     ImageCropper: () => import('@/components/molecules/ImageCropper.vue'),
-    SvgQrcode: () => import('@/components/atoms/SvgQrcode.vue')
+    SvgQrcode: () => import('@/components/atoms/SvgQrcode.vue'),
+    InputTextRoomName,
+    ComboboxTabs
   },
   setup() {
     const stepWindow = ref(0)
     const newRoom = reactive<RoomOptions>({
       name: 'New Room',
-      image: ''
+      photoURL: '',
+      tags: []
     })
 
     const valid = ref(false)
@@ -242,7 +242,7 @@ export default defineComponent({
     })
 
     const onCrop = (dataURL: string) => {
-      newRoom.image = dataURL
+      newRoom.photoURL = dataURL
     }
 
     const stepper = [
@@ -254,6 +254,10 @@ export default defineComponent({
         title: 'Image',
         icon: mdiImageEdit
       },
+      {
+        title: 'Tag',
+        icon: mdiTagPlus
+      },
       { title: 'Result', icon: '' }
     ]
 
@@ -262,17 +266,18 @@ export default defineComponent({
     })
 
     const onNext = async () => {
-      if (stepWindow.value === 2) {
+      if (stepWindow.value === 3) {
         stepWindow.value++
         // const photoURL = await blobToDataURL(newRoom.image!)
-        const photoURL = newRoom.image ? newRoom.image : ''
+        const photoURL = newRoom.photoURL ? newRoom.photoURL : ''
 
         // const photoURL = newRoom.image ? URL.createObjectURL(newRoom.image) : ''
         // console.log(photoURL)
 
         const documentData = await createRoom({
           name: newRoom.name,
-          photoURL
+          photoURL,
+          tags: newRoom.tags
         })
         url.value = `${process.env.baseUrl}/public/${documentData.id}`
         await wait(2000)
@@ -286,6 +291,7 @@ export default defineComponent({
         stepWindow.value++
       }
     }
+
     return {
       mdiClose,
       ...toRefs(newRoom),
@@ -299,8 +305,6 @@ export default defineComponent({
       stepSet,
       mdiCogs,
       mdiSquareEditOutline,
-      mdiSemanticWeb,
-      mdiCloseCircle,
       mdiFormatListText,
       mdiChatProcessing,
       mdiImageEdit,
