2f4260b5ad64beca319d0665368c4dd8970617c1
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-24 02:53:41 +0900

    :bug: (__mock__/index.ts) Remove firebase initialize

--- packages/share/__mock__/index.ts
+++ packages/share/__mock__/index.ts
@@ -1,32 +1,35 @@
-import { Profile ,PublicRoom } from "../core";
-import firebase,{ firestore } from "firebase";
+import { Profile  } from "../core";
+// import { firestore } from "firebase";
 
-firebase.initializeApp({projectId: 'test'})
+// // firebase.initializeApp({projectId: 'test'})
 
 const userName = 'authenticated'
-const timestamp = firestore.FieldValue.serverTimestamp() as firestore.Timestamp
+// const timestamp = firestore.FieldValue.serverTimestamp() as firestore.Timestamp
 export const profile:Profile = {
   displayName: userName,
   photoURL: ''
 }
 
+const timestamp = 'Friday'
 
 export const author = {
   isAnonymous: false,
-  ...profile,
-  ref: firestore().collection('profiles').doc(userName) as firestore.DocumentReference<Profile>
+  ...profile
 }
 
-export const publicRoom:PublicRoom = {
-  name: '',
+export const publicRoom = {
+  name: 'Roomy',
   photoURL: '',
   messageCount: 0,
   recent: {
-    author,
+    author: {
+      isAnonymous: true
+    },
     shortMessage: 'No message',
     kind: 'TEXT',
     updatedAt: timestamp
   },
+  tags: [ 'Public', 'Open'],
   createdAt: timestamp,
   updatedAt: timestamp
 
