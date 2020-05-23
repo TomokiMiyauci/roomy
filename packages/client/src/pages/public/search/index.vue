3f12a251e2c73e2143eb40ece95728d80743bcc9
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-23 11:54:53 +0900

    :recycle: (search/index.vue) Use outside public room card

--- packages/client/src/pages/public/search/index.vue
+++ packages/client/src/pages/public/search/index.vue
@@ -4,85 +4,24 @@
     <v-subheader>Public Rooms</v-subheader>
     <v-row>
       <v-col v-for="room in rooms" :key="room.id">
-        <v-card :to="room.id" shaped raised nuxt color="primary" dark>
-          <v-overlay :value="overlay" absolute>
-            <SvgQrcode :text="room.name" />
-            <button-close @click="overlay = false" />
-          </v-overlay>
-          <v-icon class="d-flex mx-auto" size="200px">{{ mdiNewBox }}</v-icon>
-          <v-divider />
-          <v-card-title>
-            {{ room.name }}
-            <v-spacer />
-            <ButtonQrcode @click="overlay = true" />
-          </v-card-title>
-          <v-card-subtitle>
-            <v-icon small left>{{ mdiChatProcessing }}</v-icon
-            >{{ room.messageCount }}
-          </v-card-subtitle>
-
-          <v-card-text>
-            <div>This is description</div>
-            <v-subheader>
-              <v-icon left>{{ mdiTagMultiple }}</v-icon
-              >Tags</v-subheader
-            >
-            <v-row>
-              <v-col v-for="tag in room.tags" :key="tag" cols="auto">
-                <ChipTag :text="tag" color="grey" />
-              </v-col>
-            </v-row>
-
-            <v-subheader>
-              <v-icon left>{{ mdiChatProcessing }}</v-icon> Latest message
-            </v-subheader>
-
-            <v-list two-line class="v-card--shaped">
-              <v-list-item :key="room.recent.shortMessage">
-                <v-list-item-avatar>
-                  <v-icon>{{ mdiAccountCircle }}</v-icon>
-                  <!-- <v-im/g v-else :src="room.recent.author.photoURL" /> -->
-                </v-list-item-avatar>
-
-                <v-list-item-content>
-                  <v-list-item-title>sfaff</v-list-item-title>
-                  <v-list-item-subtitle
-                    v-text="room.recent.shortMessage"
-                  ></v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-          </v-card-text>
-        </v-card>
+        <card-public-room :room="room" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
-import {
-  mdiAccountCircle,
-  mdiChatProcessing,
-  mdiNewBox,
-  mdiTagMultiple
-} from '@mdi/js'
 import { defineComponent, onBeforeMount, ref } from '@vue/composition-api'
 
-import SvgQrcode from '@/components/atoms/SvgQrcode.vue'
-import ButtonClose from '@/components/molecules/ButtonClose.vue'
-import ButtonQrcode from '@/components/molecules/ButtonQrcode.vue'
-import ChipTag from '@/components/molecules/ChipTag.vue'
+import CardPublicRoom from '@/components/organisms/CardPublicRoom.vue'
 import { getPublicRooms } from '@/repositories/publicRoom'
 import { PublicRoomMerged } from '~types/core'
 export default defineComponent({
-  components: {
-    ChipTag,
-    ButtonQrcode,
-    ButtonClose,
-    SvgQrcode
-  },
-
   watchQuery: ['q'],
+
+  components: {
+    CardPublicRoom
+  },
 
   // validate() {
   //   console.log(query)
@@ -98,7 +37,6 @@ export default defineComponent({
 
   setup(_, { root }) {
     const rooms = ref<PublicRoomMerged[]>([])
-    const overlay = ref(false)
 
     onBeforeMount(async () => {
       const keyword = root.$route.query.q as string
@@ -106,18 +44,15 @@ export default defineComponent({
       const result = await getPublicRooms(keyword)
 
       result.docs.forEach(async (doc) => {
-        rooms.value.push(await doc.data())
+        const publicRoom = await doc.data()
+
+        rooms.value.push(publicRoom)
       })
     })
 
     return {
       rooms,
-      q: root.$route.query.q,
-      mdiNewBox,
-      mdiChatProcessing,
-      mdiTagMultiple,
-      mdiAccountCircle,
-      overlay
+      q: root.$route.query.q
     }
   }
 })
