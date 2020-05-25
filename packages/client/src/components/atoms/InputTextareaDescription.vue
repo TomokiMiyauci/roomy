dd24d9a7a83a0241a2e1c8cfce47b3fd63d28a07
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-25 23:57:11 +0900

    :sparkles: (InputTextareaDescription.vue) Add input textarea for room desc

---
+++ packages/client/src/components/atoms/InputTextareaDescription.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-textarea
+    :prepend-inner-icon="mdiBookInformationVariant"
+    :clear-icon="mdiCloseCircle"
+    :value="value"
+    v-on="$listeners"
+    type="text"
+    outlined
+    clearable
+    dense
+    hint="e.g.: This is Roomy Offical Room."
+    placeholder="Enter room description"
+    auto-grow
+    rows="1"
+    label="Room Description"
+  />
+</template>
+
+<script lang="ts">
+import { mdiBookInformationVariant, mdiCloseCircle } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+export default defineComponent({
+  props: {
+    value: {
+      type: String || Number,
+      default: ''
+    }
+  },
+
+  setup() {
+    return { mdiBookInformationVariant, mdiCloseCircle }
+  }
+})
+</script>
