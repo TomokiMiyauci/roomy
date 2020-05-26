3c4212c19cb0f38732db85ba4ec1478f25909cbd
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-27 02:38:44 +0900

    :sparkles: Add login button and login suggestion

---
+++ packages/client/src/components/molecules/ButtonLogin.vue
@@ -0,0 +1,36 @@
+<template>
+  <base-button
+    v-bind="$attrs"
+    @click="$router.push(location)"
+    color="secondary"
+    icon
+  >
+    <base-icon>{{ mdiLogin }}</base-icon>
+  </base-button>
+</template>
+
+<script lang="ts">
+import { mdiLogin } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+
+import BaseButton from '@/components/atoms/BaseButton.vue'
+import BaseIcon from '@/components/atoms/BaseIcon.vue'
+
+export default defineComponent({
+  components: {
+    BaseButton,
+    BaseIcon
+  },
+
+  props: {
+    location: {
+      type: String,
+      default: '/login'
+    }
+  },
+
+  setup() {
+    return { mdiLogin }
+  }
+})
+</script>
---
+++ packages/client/src/components/molecules/PromptLogin.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row class="flex-column fill-height" justify="center" align="center">
+    <v-col cols="auto">
+      <span class="title">
+        <v-icon color="primary" left>{{ mdiInformation }}</v-icon>
+        Require Signin
+      </span>
+    </v-col>
+    <v-col cols="auto">
+      <button-login />
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { mdiInformation } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+
+import ButtonLogin from '@/components/molecules/ButtonLogin.vue'
+
+export default defineComponent({
+  components: {
+    ButtonLogin
+  },
+
+  setup() {
+    return { mdiInformation }
+  }
+})
+</script>
