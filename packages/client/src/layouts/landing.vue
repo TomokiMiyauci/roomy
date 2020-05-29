6e9b405ed41b769d18e0e459f683e1335a005239
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-05-29 23:44:53 +0900

    :sparkles: (layouts/landing.vue) Add layout for landing

---
+++ packages/client/src/layouts/landing.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-app-bar tile raised elevate-on-scroll app>
+      <v-toolbar-title>
+        Roomy
+      </v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items>
+        <v-btn text>home</v-btn>
+        <v-btn text>services</v-btn>
+        <v-btn text>contact</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-content>
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({})
+</script>
