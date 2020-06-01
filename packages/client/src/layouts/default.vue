18af6a2a493a45d21f91f1d9e88e51b3d77d570d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-06-01 20:24:54 +0900

    :fire: (default.vue) Remove layout transition

--- packages/client/src/layouts/default.vue
+++ packages/client/src/layouts/default.vue
@@ -1,9 +1,6 @@
 <template>
   <v-app>
     <the-app-bar :login="login" :photo-u-r-l="photoURL" />
-
-    <!-- <the-navigation-drawer-left />
-    <the-navigation-drawer-right /> -->
     <v-content>
       <transition name="fade">
         <nuxt />
@@ -17,8 +14,6 @@
 import { defineComponent } from '@vue/composition-api'
 
 import TheAppBar from '@/components/organisms/TheAppBar.vue'
-// import TheNavigationDrawerLeft from '@/components/organisms/TheNavigationDrawerLeft.vue'
-// import TheNavigationDrawerRight from '@/components/organisms/TheNavigationDrawerRight.vue'
 import { user } from '@/store'
 
 export default defineComponent({
@@ -41,22 +36,5 @@ export default defineComponent({
 
 .fade-enter-active {
   transition: opacity 2s;
-}
-
-.layout {
-  &-leave-active {
-    transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
-  }
-  &-leave-to {
-    transform: translateX(-10px);
-    opacity: 0;
-  }
-  &-enter {
-    transform: translateX(10px);
-    opacity: 0;
-  }
-  &-enter-active {
-    transition: all 0.3s ease;
-  }
 }
 </style>
