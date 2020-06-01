0fae9bf4b14c4ee3cf30e2fb6d154484467ef8d3
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-06-01 23:58:23 +0900

    :sparkles: (CardCreateStream.vue) Add card of create public room stream

---
+++ packages/client/src/components/organisms/CardCreateStream.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-toolbar>
+      <button-close @click="$emit('close')" />
+      <v-toolbar-title>Create Video</v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      <!-- <v-text-field /> -->
+    </v-card-text>
+    <v-divider />
+    <v-card-actions>
+      <v-spacer />
+      <v-btn color="primary">create</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+import ButtonClose from '@/components/molecules/ButtonClose.vue'
+export default defineComponent({
+  components: {
+    ButtonClose
+  }
+})
+</script>
