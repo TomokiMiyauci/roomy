5694fa527350a167bf1b4587bbb605e2d373d7dd
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-06-05 01:25:04 +0900

    :sparkles: (icons) Add mdi-chat-procesing and mdi-new-box icons

---
+++ packages/client/src/components/atoms/icons/MdiChatProcessing.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-icon v-bind="$attrs">{{ mdiChatProcessing }}</v-icon>
+</template>
+
+<script lang="ts">
+import { mdiChatProcessing } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+export default defineComponent({
+  setup() {
+    return { mdiChatProcessing }
+  }
+})
+</script>
---
+++ packages/client/src/components/atoms/icons/MdiNewBox.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-icon v-bind="$attrs">{{ mdiNewBox }}</v-icon>
+</template>
+
+<script lang="ts">
+import { mdiNewBox } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+export default defineComponent({
+  setup() {
+    return { mdiNewBox }
+  }
+})
+</script>
