01557bbc250b24109e4b8ce6dee0e36ea50d3d92
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-06-12 01:43:40 +0900

    :zap: (ssr.ts) Add cache policy

--- packages/client/functions/ssr.ts
+++ packages/client/functions/ssr.ts
@@ -9,6 +9,7 @@ export default functions
   .region('us-central1')
   .runWith({ memory: '2GB' })
   .https.onRequest(async (req, res) => {
+    res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
     await nuxt.ready()
     return nuxt.render(req, res)
   })
