4204caf8afbe7afced877143bba922ed8b4feeff
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-06-14 20:51:09 +0900

    :wrench: (nuxt.config.ts) Change global transition

--- packages/client/nuxt.config.ts
+++ packages/client/nuxt.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 // import i18n from './nuxt-i18n.config'
 
 const autoprefixer = require('autoprefixer')
-const purgecss = require('@fullhuman/postcss-purgecss')
+// const purgecss = require('@fullhuman/postcss-purgecss')
 
 const config: Configuration = {
   mode: 'universal',
@@ -68,14 +68,14 @@ const config: Configuration = {
    */
   css: ['~/assets/css/transition.scss'],
 
-  pageTransition: {
-    name: 'fade',
-    mode: 'out-in'
+  transition: {
+    name: 'fade'
   },
 
+  // pageTransition: 'fade-right',
+
   layoutTransition: {
-    name: 'fade-right',
-    mode: 'out-in'
+    name: 'fade-right'
   },
   /*
    ** Plugins to load before mounting the App
@@ -179,70 +179,71 @@ const config: Configuration = {
    ** Build configuration
    */
   build: {
+    cache: true,
     extractCSS: process.env.NODE_ENV === 'production',
     postcss: {
       plugins: [
-        autoprefixer({ grid: 'autoplace' }),
-        purgecss({
-          content: [
-            path.join(__dirname, './src/**/*.ts'),
-            path.join(__dirname, './src/**/*.vue'),
-            path.join(__dirname, './src/**/*.js'),
-            path.join(__dirname, './node_modules/vuetify/src/**/*.ts'),
-            './node_modules/vuetify/dist/vuetify.js'
-          ],
-          whitelist: [
-            'html',
-            'body',
-            'nuxt-progress',
-            'spacer',
-            'primary',
-            'secondary',
-            'accent',
-            'error',
-            'warning',
-            'info',
-            'success',
-            'icon',
-            'fab',
-            'skeleton',
-            'v-application',
-            'v-application--wrap',
-            'button',
-            'input',
-            'select',
-            'textarea',
-            'container',
-            'row',
-            'no-gutters',
-            'col',
-            'col-auto',
-            'theme'
-          ],
-          whitelistPatterns: [
-            /^v-/,
-            /^theme-/,
-            /^application--/,
-            /(col|row|fill-height)/,
-            /^icon/,
-            /^(d-)/,
-            /^(text)/,
-            /.*-transition/,
-            /^_/,
-            /^v-((?!application).)*$/,
-            /^theme--*/,
-            /^container/
-          ],
-          whitelistPatternsChildren: [
-            /^v-/,
-            /^theme-/,
-            /^application--/,
-            /(col|row|fill-height)/,
-            /^icon/,
-            /^v-((?!application).)*$/,
-            /^theme--*/
-          ]
-        })
+        autoprefixer({ grid: 'autoplace' })
+        // purgecss({
+        //   content: [
+        //     path.join(__dirname, './src/**/*.ts'),
+        //     path.join(__dirname, './src/**/*.vue'),
+        //     path.join(__dirname, './src/**/*.js'),
+        //     path.join(__dirname, './node_modules/vuetify/src/**/*.ts'),
+        //     './node_modules/vuetify/dist/vuetify.js'
+        //   ],
+        //   whitelist: [
+        //     'html',
+        //     'body',
+        //     'nuxt-progress',
+        //     'spacer',
+        //     'primary',
+        //     'secondary',
+        //     'accent',
+        //     'error',
+        //     'warning',
+        //     'info',
+        //     'success',
+        //     'icon',
+        //     'fab',
+        //     'skeleton',
+        //     'v-application',
+        //     'v-application--wrap',
+        //     'button',
+        //     'input',
+        //     'select',
+        //     'textarea',
+        //     'container',
+        //     'row',
+        //     'no-gutters',
+        //     'col',
+        //     'col-auto',
+        //     'theme'
+        //   ],
+        //   whitelistPatterns: [
+        //     /^v-/,
+        //     /^theme-/,
+        //     /^application--/,
+        //     /(col|row|fill-height)/,
+        //     /^icon/,
+        //     /^(d-)/,
+        //     /^(text)/,
+        //     /.*-transition/,
+        //     /^_/,
+        //     /^v-((?!application).)*$/,
+        //     /^theme--*/,
+        //     /^container/
+        //   ],
+        //   whitelistPatternsChildren: [
+        //     /^v-/,
+        //     /^theme-/,
+        //     /^application--/,
+        //     /(col|row|fill-height)/,
+        //     /^icon/,
+        //     /^v-((?!application).)*$/,
+        //     /^theme--*/
+        //   ]
+        // })
       ]
     },
 
