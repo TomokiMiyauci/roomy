7847acb99fbf4a59c58dd4575d7c683534bcce3e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-06-22 23:15:50 +0900

    :sparkles: (icons/MdiEye.vue) Add icon of mdi-eye

---
+++ packages/client/src/components/atoms/icons/MdiEye.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-icon v-bind="$attrs">{{ mdiEye }}</v-icon>
+</template>
+
+<script lang="ts">
+import { mdiEye } from '@mdi/js'
+import { defineComponent } from '@vue/composition-api'
+export default defineComponent({
+  setup() {
+    return { mdiEye }
+  }
+})
+</script>
